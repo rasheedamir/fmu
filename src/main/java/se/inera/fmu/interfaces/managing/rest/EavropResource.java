1dca6cf16ca932353229ab255e04bd2536d9690a
nickiewooster@gmail.com <freeway86>
2014-09-02 12:57:01 +0200

    rest mock ongoing...

--- src/main/java/se/inera/fmu/interfaces/managing/rest/EavropResource.java
+++ src/main/java/se/inera/fmu/interfaces/managing/rest/EavropResource.java
@@ -1,35 +1,44 @@
 package se.inera.fmu.interfaces.managing.rest;
 
-import com.codahale.metrics.annotation.Timed;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import se.inera.fmu.domain.model.eavrop.EavropRepository;
+
+import se.inera.fmu.domain.model.eavrop.Eavrop;
+import se.inera.fmu.domain.model.eavrop.EavropRepositoryStub;
 import se.inera.fmu.interfaces.managing.rest.dto.EavropDTO;
 
-import javax.inject.Inject;
-import java.util.List;
+import com.codahale.metrics.annotation.Timed;
 
 /**
  * Created by Rasheed on 8/25/14.
  *
  * REST controller for tracking eavrops
  */
-@SuppressWarnings("ALL")
+@SuppressWarnings("all")
 @RestController
 @RequestMapping("/app")
 public class EavropResource {
 
     @Inject
-    private EavropRepository eavropRepository;
+    private EavropRepositoryStub eavropRepository;
 
     @RequestMapping(value = "/rest/eavrop",
         method = RequestMethod.GET,
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
     public ResponseEntity<List<EavropDTO>> getEavrops(){
-        return null;
+    	ResponseEntity<List<EavropDTO>> list = new ResponseEntity<List<EavropDTO>>(HttpStatus.OK);
+    	for (Eavrop e : eavropRepository.findAll()) {
+			
+		}
+        return list;
     }
 }
