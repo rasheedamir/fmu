09e6e23c0c8573201c91f59191ae47e9fb97dcf9
nickiewooster@gmail.com <freeway86>
2014-09-17 13:57:45 +0200

    Fixed fonts not copied to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -308,7 +308,7 @@ module.exports = function (grunt) {
           src: ['generated/*']
         }, {
           expand: true,
-          cwd: 'bower_components/bootstrap/dist',
+          cwd: '<%= yeoman.app %>/bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
         }]
