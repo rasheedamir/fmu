6fd035d05a45327badf3ee9df4bb7ad606fbad03
hjortling <rickard.hjortling@r2m.se>
2014-09-22 18:34:08 +0200

    FMU-17 Import statement cleanup

--- src/main/java/se/inera/fmu/domain/party/Party.java
+++ src/main/java/se/inera/fmu/domain/party/Party.java
@@ -3,21 +3,13 @@ package se.inera.fmu.domain.party;
 import java.io.Serializable;
 
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import org.apache.commons.lang3.Validate;
-
-import se.inera.fmu.domain.model.bestallare.Bestallare;
-import se.inera.fmu.domain.model.bestallare.PersonalId;
-import se.inera.fmu.domain.shared.AbstractBaseEntity;
 	
 @Entity
 @Table(name = "T_PARTY")
@@ -77,10 +69,4 @@ public abstract class Party implements Serializable  {
 	private void setOrganistation(String organistation) {
 		this.organistation = organistation;
 	}
-    
-
-	
-	
-	
-	
 }
