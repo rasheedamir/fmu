9fd3570cc5233206f8fdeaca2ade4a9e73dc6af7
Quang vu <nickiewooster@gmail.com>
2015-01-19 15:49:36 +0100

    added fmu-responsive-menu for toggling menus

---
+++ src/main/webapp/scripts/Directives/Fmu-responsive-menu.js
@@ -0,0 +1,22 @@
+'use strict';
+angular.module('fmuClientApp')
+    .directive('fmuResponsiveMenu', ['$compile', function ($compile) {
+        return {
+            restrict: 'A',
+            link: function (scope, iElement) {
+                var element = angular.element('<div class="navigation-collapsed"><button ng-click="expandMenu()" class="fa fa-bars"></button></div>');
+                var menuElement = $compile(element)(scope);
+                iElement.prepend(menuElement);
+
+                scope.expandMenu = function(){
+                    var menus = iElement.find('.navigation-expanded');
+                    var oldCss = menus.css('display');
+                    if(oldCss == 'none'){
+                        menus.css('display', 'block');
+                    } else {
+                        menus.css('display', 'none');
+                    }
+                };
+            }
+        };
+    }]);
