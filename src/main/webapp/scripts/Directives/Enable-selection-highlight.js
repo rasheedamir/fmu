2cf10a6a7f9d6020562129f4e1875b934cd399e8
Quang vu <nickiewooster@gmail.com>
2015-01-21 11:13:29 +0100

    added tabs and menu selection highlight directve

--- src/main/webapp/scripts/Directives/Enable-selection-highlight.js
+++ src/main/webapp/scripts/Directives/Enable-selection-highlight.js
@@ -1,10 +1,28 @@
 'use strict';
-angular.module('fmuClientApp')
-    .directive('enableSelectionHighlight', [ function () {
+angular.module('fmuClientApp').
+directive('enableSelectionHighlight', [function() {
         return {
-            restrict: 'C',
-            link: function (scope, iElement) {
-                var originColor = iElement.find('li').css('background-color');
-                console.log(iElement.find('li').)
-        }};
-    }]);
+            restrict: 'A',
+            link: function($scope, iElm, iAttrs) {
+                var selectionClass = iAttrs.enableSelectionHighlight;
+                var tabs = iElm.find('li');
+                $scope.$watch($scope.$last, function() {
+                    tabs = iElm.find('li');
+                    var defaultSelection = iAttrs.highlightTab - 1;
+                    if (tabs.eq(defaultSelection)) {
+                        tabs.eq(defaultSelection).addClass(selectionClass);
+                    }
+                });
+
+                iElm.on('click', 'li', function() {
+                    if (selectionClass !== null) {
+                        angular.forEach(tabs, function(element) {
+                            angular.element(element).removeClass(selectionClass);
+                        });
+                        angular.element(this).toggleClass(selectionClass);
+                    }
+                });
+            }
+        };
+    }
+]);
